59335b638c4afd6c2d9b659b299cd5aa197d6441
Michael Waide <mvwaide@gmail.com>
2020-06-22 16:43:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [red,orange,yellow,green]
+  ["red","orange","yellow","green"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+<< "blue"
 end
 
 def add_element_to_start_of_array(array, element)
